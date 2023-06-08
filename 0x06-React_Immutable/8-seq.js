e3ab5f50375011cbcd74f637e821b516651bc2c4
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2023-06-08 14:05:51 +0000

    Task 8

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,7 +1,30 @@
-import immutable from 'immutable';
+import Immutable from 'immutable';
 
-const { Seq } = immutable;
+function printBestStudents(students) {
+  // filters students by their score > 70
+  // and prints to the console the student with
+  // the first letter in their 'firstName' and 'lastName' properties in uppercase.
+  //
+  // students - Object containing students
+  //
+  // returns nothing
+  const mySeq = Immutable.Seq(students);
+  console.log(mySeq);
 
-export default function printBestStudents() {
-    
+  const filtered = mySeq.filter((student) => student.score > 70);
+
+  const capFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    const student = JSObject[key];
+    student.firstName = capFirstLetter(student.firstName);
+    student.lastName = capFirstLetter(student.lastName);
+    return student;
+  });
+
+  console.log(JSObject);
 }
+
+export default printBestStudents;
